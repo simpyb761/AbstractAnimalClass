96920b2f982595339d0df9ac7c3c731cbd86aa53
simpyb761 <simpyb761@gmail.com>
2023-02-22 16:36:32 -0500

    Completed Program

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+
+namespace AbstractAnimalClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reptile firstReptile = new Reptile();
+            firstReptile.Name = "Chi";
+            firstReptile.Species = "Bearded Dragon";
+            firstReptile.NumberOfLegs = 4;
+            Reptile secondReptile = new Reptile("Boris", "Tortoise", 4);
+            WriteLine(firstReptile.whatAmI());
+            WriteLine(firstReptile.Describe());
+            WriteLine(secondReptile.whatAmI());
+            WriteLine(secondReptile.Describe());
+        }
+    }
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public abstract string Describe();
+        public string whatAmI() { return "I am an animal"; }
+    }
+    class Reptile : Animal
+    {
+        public override string Name { get; set; }
+        public string Species { get; set; }
+        public int NumberOfLegs { get; set; }
+        public Reptile() 
+        {
+            Name= string.Empty;
+            Species= string.Empty;
+            NumberOfLegs= 0;
+        }
+        public Reptile(string name, string species, int numberOfLegs)
+        {
+            Name = name;
+            Species= species;
+            NumberOfLegs = numberOfLegs;
+        }
+
+        public override string Describe()
+        {
+            return $"My Name is {Name}. I am a {Species} with {NumberOfLegs} legs";
+
+        }
+    }
+}
